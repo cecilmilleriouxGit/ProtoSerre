69b96ba460924cd09a4afd5be9d346fceb20d656
Cecil MILLERIOUX <cecil.millerioux-stagiaire@laposte.fr>
2020-04-23 18:57:16 +0200

    first commit

---
+++ src/main/java/com/proto/serre/SerreApplication.java
@@ -0,0 +1,13 @@
+package com.proto.serre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proto/serre/SerreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.serre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
