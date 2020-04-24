fd40c51c2a15b9f3bf0251fa4a5b5cfc934f88f3
cecil <cecil.millerioux@gmail.com>
2020-04-24 08:18:27 +0200

    add Dht22 entity

--- src/main/java/com/proto/serre/entities/Dht22.java
+++ src/main/java/com/proto/serre/entities/Dht22.java
@@ -1,4 +1,48 @@
 package com.proto.serre.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Dht22 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private float temperature;
+    private float humidity;
+
+    public Dht22() {
+    }
+
+    public Dht22(float temperature, float humidity) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
 }
