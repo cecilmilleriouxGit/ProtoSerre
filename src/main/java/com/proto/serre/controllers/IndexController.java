99b49808b51f8aa284581478b6c129c15bbd75f8
cecil <cecil.millerioux@gmail.com>
2020-04-27 15:10:02 +0200

    save for restart, arrayList is not ok

--- src/main/java/com/proto/serre/controllers/IndexController.java
+++ src/main/java/com/proto/serre/controllers/IndexController.java
@@ -29,39 +29,47 @@ public class IndexController {
     public String index(Model model){
         List<DateOfSave> allDateOfSave = dateOfSaveRepository.findAll();
         ArrayList arrList = new ArrayList();
-        ArrayList localDateList = new ArrayList();
-        for(int count = 1; count < 7; count++){
+        ArrayList localDateList1 = new ArrayList();
+        ArrayList localDateList2 = new ArrayList();
+        ArrayList localDateList3 = new ArrayList();
+        ArrayList localDateList4 = new ArrayList();
+        ArrayList localDateList5 = new ArrayList();
+        ArrayList localDateList6 = new ArrayList();
+        ArrayList localDateList7 = new ArrayList();
+        int count = 0;
             for(DateOfSave dateOfSaveToArrayList : allDateOfSave){
-                if(arrList.isEmpty()){
-                    arrList.add(localDateList);
-                }
-//                if(!localDateList.isEmpty()){
-//                    if(!dateOfSaveToArrayList.equals(localDateList.get(count-1))){
-//                        localDateList.add(dateOfSaveToArrayList.getDate());/*.format(DateTimeFormatter.ofPattern("dd-MM-yyy")))*/
-//                        for(Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()){
-//                            localDateList.add(dht22.getTemperature());
-//                            localDateList.add(dht22.getHumidity());
+                if(count < 7){
+                    arrList.add(localDateList1);
+
+                        localDateList1.add(dateOfSaveToArrayList.getDate());
+                        for(Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()){
+                            localDateList1.add(dht22.getTemperature());
+                            localDateList1.add(dht22.getHumidity());
+                        }
+                        count += 1;
+
+//                    if(!localDateList.isEmpty()){
+//                        if(count > 1){
+//                            localDateList.add(dateOfSaveToArrayList.getDate());
+//                            for(Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()){
+//                                localDateList.add(dht22.getTemperature());
+//                                localDateList.add(dht22.getHumidity());
+//                            }
+//                            count += 1;
 //                        }
 //                    }
-//                }
-                if(localDateList.isEmpty()){
-                    localDateList.add(dateOfSaveToArrayList.getDate());/*.format(DateTimeFormatter.ofPattern("dd-MM-yyy")))*/
-                    for(Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()){
-                        localDateList.add(dht22.getTemperature());
-                        localDateList.add(dht22.getHumidity());
-                    }
                 }
-//                if(dateOfSaveToArrayList.equals(localDateList.get(count))){
+            }
+        //                if(dateOfSaveToArrayList.equals(localDateList.get(count))){
 //                    for(Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()){
 //                        localDateList.add(dht22.getTemperature());
 //                        localDateList.add(dht22.getHumidity());
 //                    }
-                }
-            }
+
 
 
         System.out.println(arrList);
-        System.out.println(localDateList.size());
+        System.out.println(localDateList1.size());
 
         return "index";
     }
--- src/main/java/com/proto/serre/repositories/DateOfSaveRepository.java
+++ src/main/java/com/proto/serre/repositories/DateOfSaveRepository.java
@@ -3,14 +3,7 @@ package com.proto.serre.repositories;
 import com.proto.serre.entities.DateOfSave;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
