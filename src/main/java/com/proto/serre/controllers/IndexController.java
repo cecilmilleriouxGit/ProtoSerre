52974a0634e5fe761604d9da6e81c4e9dbf86fd3
cecil <cecil.millerioux@gmail.com>
2020-04-28 11:29:55 +0200

    save thymeleaf

--- src/main/java/com/proto/serre/controllers/IndexController.java
+++ src/main/java/com/proto/serre/controllers/IndexController.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.thymeleaf.expression.Lists;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -31,24 +30,41 @@ public class IndexController {
     public String index(Model model) {
         List<DateOfSave> allDateOfSave = dateOfSaveRepository.findByDate();
         ArrayList arrList = new ArrayList();
-
-        int count = 0;
-
-
+        int countTemperature = 0;
+        // import temperature
         for (DateOfSave dateOfSaveToArrayList : allDateOfSave) {
-            if (count < 7) {
+            if (countTemperature < 7) {
                 ArrayList arr = new ArrayList();
                 arrList.add(arr);
-
-                arr.add(dateOfSaveToArrayList.getDate());
+                SimpleDateFormat formater = new SimpleDateFormat("(yyyy, MM, dd)");
+//                arr.add(formater.format(dateOfSaveToArrayList.getDate()));
                 for (Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()) {
-                    arr.add(dht22.getTemperature());
-                    arr.add(dht22.getHumidity());
+                    arr.add(formater.format(dateOfSaveToArrayList.getDate())+ ", y:" + dht22.getTemperature());
                 }
-                count += 1;
-                model.addAttribute("arrayList"+count, arr);
-                System.out.println("arrayList"+count);
-                System.out.println(arr);
+                countTemperature += 1;
+                model.addAttribute("arrayListTemperature"+countTemperature, arr);
+//                System.out.println("arrayListTemperature"+countTemperature);
+//                System.out.println(arr);
+            }
+        }
+//        model.addAttribute("arrayListTemperature1", arrList.get(0));
+
+        System.out.println(arrList.get(0));
+        int countHumidity = 0;
+        // import humidity
+        for (DateOfSave dateOfSaveToArrayList : allDateOfSave) {
+            if (countHumidity < 7) {
+                ArrayList arr = new ArrayList();
+                arrList.add(arr);
+                SimpleDateFormat formater = new SimpleDateFormat("(dd, MM, yy)");
+//                arr.add(formater.format(dateOfSaveToArrayList.getDate()));
+                for (Dht22 dht22 : dateOfSaveToArrayList.getDht22Set()) {
+                    arr.add(formater.format(dateOfSaveToArrayList.getDate()) + ", y:" + dht22.getHumidity());
+                }
+                countHumidity += 1;
+                model.addAttribute("arrayListHumidity"+countHumidity, arr);
+//                System.out.println("arrayListHumidity"+countHumidity);
+//                System.out.println(arr);
             }
         }
         return "index";
